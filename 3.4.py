da1092f82bae31ec6f43a022ec0b7c743f7be618
aishwarya-2009 <gaishwarya709@gmail.com>
2025-10-27 18:49:08 +0530

    Add files via upload

---
+++ 3.4.py
@@ -0,0 +1,7 @@
+import random
+fortunes=["Good things come those who wait.",
+          "Patience is a virtue.",
+          "The early bird gets the worm.",
+          "A wise man once said, everything in its own time and place.",
+          "Fortune cookies rarely share fortunes."]
+print(random.choice(fortunes))
