15ed1dba728f34030ecae8e8516808e4ba142e72
aishwarya-2009 <gaishwarya709@gmail.com>
2025-10-27 18:47:37 +0530

    Add files via upload

---
+++ 3.1.py
@@ -0,0 +1,7 @@
+import math
+print("logarithm",math.log10(13))
+print("power",math.pow(10,2))
+print("floor",math.floor(10.25201))
+print("ceil",math.ceil(10.2501))
+print("absolute",abs(-10.024))
+print("factorial",math.factorial(5))
